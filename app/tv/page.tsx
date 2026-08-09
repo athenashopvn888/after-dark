ce03a1c063e81bc9cd60f0a623ed3cd3a6b33838
athenashopvn888 <athenashopvn888@gmail.com>
2026-08-10 01:37:37 +0700

    Fix three-ounce deal display price

--- app/tv/page.tsx
+++ app/tv/page.tsx
@@ -671,7 +671,7 @@ const TICKER_SLIDES = [
   "Flower Tiers and Menu Categories",
   "Open 24 Hours",
   "ALL SALES ARE FINAL",
-  "🎮 Play Games at afterdarkcannabis.com/games",
+  "DELIVERY MENU at afterdarkcannabis.com/delivery",
 ];
 
 function VerticalTicker() {
